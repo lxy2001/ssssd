12f78a3893f2edd73feb0f8b39f4e58bd6fdc69e
20181106279 <1771478860@qq.com>
2019-09-11 11:39:06 +0800

    Initial Commit

---
+++ ssssd/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ssssd
+//
+//  Created by 20181106279 on 2019/9/11.
+//  Copyright © 2019 20181106279. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
